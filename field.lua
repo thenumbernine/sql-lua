e1da279c7983863981c44242e61031f005bde1c6
thenumbernine <christopher.e.moore@gmail.com>
2024-06-21 12:07:22 +0600

    first commit

---
+++ field.lua
@@ -0,0 +1,40 @@
+local class = require 'ext.class'
+
+local SQLField = class()
+
+SQLField.__name = 'SQLField'
+
+function SQLField:init(args)
+	for k,v in pairs(args) do
+		self[k] = v
+	end
+end
+
+function SQLField:addcmd()
+	local s = self.name
+		.. ' ' .. self.type
+	if self.attr then
+		s = s .. ' ' .. self.attr
+	end
+	return s
+end
+
+function SQLField:serializeValue(v)
+	if self.type == 'text'
+	or self.type:match'^varchar'
+	then
+		return ('%q'):format(v)
+	end
+
+	if self.type == 'timestamp' then
+		-- TODO decimals?
+		--local t = os.date('*t', v)
+		--return os.date("!'%Y-%m-%d %H:%M:%S'", v)
+		-- or do I assume it's a date string?
+		return ('%q'):format(v)
+	end
+
+	return v
+end
+
+return SQLField
---
+++ table.lua
@@ -0,0 +1,87 @@
+local table = require 'ext.table'
+local class = require 'ext.class'
+local SQLField = require 'sql.field'
+
+local SQLTable = class()
+
+SQLTable.__name = 'SQLTable'
+
+function SQLTable:init(args)
+	self.name = assert(args.name)
+	self.fields = table((assert(args.fields)))
+	self.constraints = args.constraints
+
+	for i=1,#self.fields do
+		local field = SQLField(self.fields[i])
+		self.fields[i] = field
+		-- TODO separate table for this? meh?
+		self.fields[field.name] = field
+	end
+end
+
+
+-- generate a drop cmd
+function SQLTable:dropcmd()
+	return 'drop table '..self.name..';'
+end
+
+-- generate create
+function SQLTable:createcmd()
+	local ss = self.fields:mapi(function(field) return field:addcmd() end)
+	if self.constraints then
+		for _,constraint in ipairs(self.constraints) do
+			ss:insert(constraint)
+		end
+	end
+	return 'create table '..self.name..' ('..ss:concat', '..');'
+end
+
+--[[
+generate insert
+obj = keys and values to be inserted
+	value sql formatting is done by matching the keys with the sql fields and formatting according to field type
+--]]
+function SQLTable:insertcmd(obj)
+	local keys = table()
+	local values = table()
+	for k,v in pairs(obj) do
+		keys:insert(k)
+		local field = assert(self.fields[k])
+		values:insert((field:serializeValue(v)))
+	end
+	return 'insert into '..self.name
+		..' ('..keys:concat', '..') values ('
+		..values:concat', '..');'
+end
+
+-- Make table:cmd functions to interact with whatever API is available first
+
+local function wrap_resty(field)
+	local fieldcmd = field..'cmd'
+	SQLTable[field] = function(self, db, ...)
+		local cmd = self[fieldcmd](self, ...)
+ngx.log(ngx.ERR, '> '..cmd)
+		return db:query(cmd)
+	end
+end
+
+-- based on luasql-mysql (since it's run CLI):
+local function wrap_luasql(field)
+	local fieldcmd = field..'cmd'
+	SQLTable[field] = function(self, conn, ...)
+		local cmd = self[fieldcmd](self, ...)
+print('> '..cmd)
+		return conn:execute(cmd)
+	end
+end
+
+local usingResty = pcall(require, 'resty.mysql')
+for _,field in ipairs{'drop', 'create', 'insert'} do
+	if usingResty then
+		wrap_resty(field)
+	else
+		wrap_luasql(field)
+	end
+end
+
+return SQLTable
